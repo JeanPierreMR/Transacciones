fe337843b452bd5fb57fe4d8fc7f93ef34529fc9
Jean <jpmejicanos@ufm.edu>
2020-02-02 20:51:31 -0600

    added push many
    adds more than one at the same time

--- src/main/java/com/example/demo/InterfazAPI.java
+++ src/main/java/com/example/demo/InterfazAPI.java
@@ -1,8 +1,10 @@
 package com.example.demo;
 
 
+import com.google.gson.*;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -11,11 +13,24 @@ public class InterfazAPI {
     InterfazAPI(){
         transaccionManager = new TransaccionManger();
     }
-    @PostMapping("/push")
+
+    @PostMapping("/Push")
     public boolean push(String operacion, double monto){
-        transaccionManager.createTransaccion(operacion, monto);
+        return transaccionManager.createTransaccion(operacion, monto);
+    }
+
+    @PostMapping("/PushMany")
+    public boolean pushMany( String data){
+        Gson g = new Gson();
+        JsonObject jsonData = g.fromJson(data, JsonObject.class);
+        for(int x=0; x<jsonData.getAsJsonArray("tipo").size(); x++){
+            if(transaccionManager.createTransaccion(jsonData.getAsJsonArray("tipo").get(x).getAsString(), jsonData.getAsJsonArray("monto").get(x).getAsInt()) == false){
+                return false;
+            }
+        }
         return true;
     }
+
     @GetMapping("/Saldo")
     public double pop(){
         return transaccionManager.getSaldo();
--- src/main/java/com/example/demo/TransaccionManger.java
+++ src/main/java/com/example/demo/TransaccionManger.java
@@ -16,25 +16,47 @@ public class TransaccionManger {
         transacciones = new Transaccion[200];
     }
     private void resizeTransacciones(double percentage){
-        /*Resizes de size of the array*/
+        /*Resizes de size of the array
+        * it creates a bigger array and copy the data there
+        * then it changes the old array*/
         int size;
-        size = (int)Math.floor(num_transacciones * percentage);
+        size = (int)Math.floor(num_transacciones * percentage);//makes the new size percentage bigger
         Transaccion temporalTransacciones[] = new Transaccion[size];
         System.arraycopy(transacciones, 0, temporalTransacciones, 0, num_transacciones);
         transacciones = temporalTransacciones;
     }
 
-    public void createTransaccion(String tipo, double monto){
+
+    public boolean createTransaccion(String tipo, double monto){
         if(num_transacciones >= transacciones.length - 5){
             this.resizeTransacciones(1.2);
         }
+        if(tipo.equals("credito")){
+            total_credito +=  monto;
+            transacciones_credito++;
+        }else if (tipo.equals("debito")){
+            total_debito +=  monto;
+            transacciones_debito++;
+        }else{
+            return false;
+        }
         transacciones[num_transacciones] = new Transaccion(tipo, monto, num_transacciones);
+        return true;
     }
 
 
     public double getSaldo(){
         return total_credito - total_debito;
     }
+//    public String getMonto_Mayor(){
+//
+//    }
+    //ingresar 1 o mas debitos creditos con auto
+    //total de cada uno+promedio de debitos
+    //monto de debitos mas grande
+    //cuantas operaciones
+    //imprimir los arreglos
+    //eliminar creditos xyz y ejecutar cdefgh
     public int getTransacciones_credito() {
         return transacciones_credito;
     }
